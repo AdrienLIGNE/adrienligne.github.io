a1fde90810aa9ebc8bf4d7f1f5bcf8d83f415b4e
adrien <adrien.ligne@etu.univ-grenoble-alpes.fr>
2023-11-23 01:19:13 +0100

    feat: first dev commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
@@ -7,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          SiTe WEb De aDrIeN
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
